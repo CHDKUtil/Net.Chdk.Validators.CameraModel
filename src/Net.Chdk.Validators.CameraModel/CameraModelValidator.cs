0512a1680c282b1c8b00f103f62485e1813a1306
Dmitry Shechtman <damnian@gmail.com>
2017-05-02 02:44:49 +0300

    Add solution

---
+++ src/Net.Chdk.Validators.CameraModel/CameraModelValidator.cs
@@ -0,0 +1,40 @@
+﻿using Net.Chdk.Model.CameraModel;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace Net.Chdk.Validators.CameraModel
+{
+    sealed class CameraModelValidator : IValidator<CameraModelInfo>
+    {
+        public void Validate(CameraModelInfo cameraModel, string basePath)
+        {
+            if (cameraModel == null)
+                throw new ArgumentNullException(nameof(cameraModel));
+
+            Validate(cameraModel.Version);
+            Validate(cameraModel.Names);
+        }
+
+        private static void Validate(Version version)
+        {
+            if (version == null)
+                throw new ValidationException("Null version");
+
+            if (version.Major < 1 || version.Minor < 0)
+                throw new ValidationException("Invalid version");
+        }
+
+        private static void Validate(string[] names)
+        {
+            if (names == null)
+                throw new ValidationException("Null names");
+
+            if (names.Length == 0)
+                throw new ValidationException("Empty names");
+
+            if (names.Any(string.IsNullOrEmpty))
+                throw new ValidationException("Missing name");
+        }
+    }
+}
---
+++ src/Net.Chdk.Validators.CameraModel/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Net.Chdk.Model.CameraModel;
+
+namespace Net.Chdk.Validators.CameraModel
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddCameraModelValidator(this IServiceCollection serviceCollection)
+        {
+            return serviceCollection
+                .AddSingleton<IValidator<CameraModelInfo>, CameraModelValidator>();
+        }
+    }
+}
